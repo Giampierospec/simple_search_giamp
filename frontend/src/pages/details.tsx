e7383485d0819ece6233ad0e0a76a262666cc14b
Giampiero Specogna <giampiero@analogrepublic.com>
2023-02-28 16:21:39 -0400

    feature: details page

--- frontend/src/pages/details.tsx
+++ frontend/src/pages/details.tsx
@@ -1,4 +1,25 @@
+import CustomLink from '../components/CustomLink'
+import { useParams } from 'react-router-dom'
+import React, { useContext, useEffect, useMemo, useState } from 'react'
+import { SearchResultContext } from '../context/searchResultContext'
+import { SearchResult } from '../interfaces/searchResults'
 const Details: React.FC = () => {
-  return <div></div>
+  const { id } = useParams()
+  const { results } = useContext(SearchResultContext)
+
+  const result = useMemo(() => {
+    return results.find((x) => x._id == id) ?? null
+  }, [results, id])
+  return (
+    <div className="flex flex-col gap-y-4 p-8 items-center">
+      <CustomLink to="/">Go back to search results</CustomLink>
+      {result && (
+        <React.Fragment>
+          <img src={`/images${result.photo}`} />
+          <p className="text-lg xl:text-2xl">{result.description}</p>
+        </React.Fragment>
+      )}
+    </div>
+  )
 }
 export default Details
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,12 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Container from '../components/Container'
 import MainPage from '../pages'
+import Details from '../pages/details'
 export const routes = createBrowserRouter([
   {
     path: '/',
     element: (
       <Container>
         <MainPage />
+      </Container>
+    ),
+  },
+  {
+    path: '/:id',
+    element: (
+      <Container>
+        <Details />
       </Container>
     ),
   },
